4e453450f30fb22a1b87219ce2ed170a364421d6
fyuneru0830 <36604859+fyuneru0830@users.noreply.github.com>
2020-07-10 21:07:51 +0900

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-sudo yum install -y wget
 sudo wget https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
-echo -n "Your Domain（Example: xxx.com)"
+echo -n "Your Domain（Example: xxx.com):"
 read DOMAIN
 ./certbot-auto --server https://acme-v02.api.letsencrypt.org/directory -d "*.${DOMAIN}" -d "${DOMAIN}" --manual --preferred-challenges dns-01 certonly
