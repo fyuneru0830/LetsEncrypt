5d434599d0019243e49503fef7e4df4ac8daaa5e
fyuneru0830 <36604859+fyuneru0830@users.noreply.github.com>
2020-07-10 21:25:38 +0900

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+yum install -y screen nano unzip zip vim
+screen -S install
 sudo wget https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
 echo -n "Your Domain（Example: xxx.com):"
 read DOMAIN
 ./certbot-auto --server https://acme-v02.api.letsencrypt.org/directory -d "*.${DOMAIN}" -d "${DOMAIN}" --manual --preferred-challenges dns-01 certonly
+cp "/etc/letsencrypt/live/${DOMAIN}/cert.pem" "/root/cert.pem"
+cp "/etc/letsencrypt/live/${DOMAIN}/privkey.pem" "privkey.pem"
+echo "Success! Cert and Key in /root/"
